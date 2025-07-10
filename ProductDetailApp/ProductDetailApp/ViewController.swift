0f20d9dd2a66c0acbf9134aac68001d257e54a57
Emincan Antalyali <antalyalie21@outlook.com>
2025-07-10 13:14:41 +0300

    project init commit

---
+++ ProductDetailApp/ProductDetailApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProductDetailApp
+//
+//  Created by Emincan Antalyalı on 10.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
